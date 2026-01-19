504f2e698d79ac8f3592c28a20777600f1c1a311
maanika <kennethmaanika@gmail.com>
2026-01-19 10:32:54 +1030

    Assignment 2
    - writer c application
    - makefile for native and cross compile builds
    - finder-test.sh updates to build and use the writer.c app
    - cross-compile.txt file to show version and path of installed cross
      compiler
    - fileresult.txt to show result of file run on writer.c after
      cross compiling

---
+++ finder-app/writer.c
@@ -0,0 +1,53 @@
+#include <syslog.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+{
+	openlog(NULL, 0, LOG_USER);
+       
+	if (argc != 3) {
+		syslog(LOG_ERR, "Invalid number of arguments (%d), expected 2", argc);
+		closelog();
+		return 1;
+	}
+
+	const char* writefile = argv[1];
+	const char* writestr = argv[2];
+
+	syslog(LOG_INFO, "Writing \"%s\" to %s", writestr, writefile);
+
+	int fd;
+	fd = open(writefile, O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd == -1) {
+ 		syslog(LOG_ERR, "Failed to open file");
+		closelog();
+		return 1;
+	}
+
+
+	ssize_t nr;
+	nr = write(fd, writestr, strlen(writestr));
+	if (nr == -1) {
+		syslog(LOG_ERR, "Failed to write to file");
+		if (close(fd) == -1) {
+			syslog(LOG_ERR, "Failed to close file");
+		}
+		closelog();
+		return 1;
+	}
+
+	if (close(fd) == -1) {
+		syslog(LOG_ERR, "Failed to close file");
+		closelog();
+		return 1;
+	}
+	
+	closelog();
+	return 0;
+}
+
+
